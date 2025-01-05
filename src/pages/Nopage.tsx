8241855b287225e2bfbfe3870cd191e5c6102e6e
Nath9666 <nathan.morel@efrei.net>
2025-01-05 13:22:08 +0100

    page 404 translation

--- src/pages/Nopage.tsx
+++ src/pages/Nopage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const NoPage: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-col items-center justify-center h-screen text-gray-50">
       <div className="w-1/2 text-center items-center rounded-3xl justify-center bg-blue-900/10 backdrop-blur p-6">
-        <h1 className="text-4xl font-bold mb-2">404 - Page Not Found</h1>
+        <h1 className="text-4xl font-bold mb-2">{t("404")}</h1>
         <p className="text-lg m-2">
-          Sorry, the page you are looking for does not exist.
+          {t("404_desc")}
         </p>
       </div>
     </div>
